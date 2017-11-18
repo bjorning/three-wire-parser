9b837240db39fd74c768b1d41fac56fbb23014f1
bjorning <bjorn.inge.hanssen@gmail.com>
2017-11-19 00:44:46 +0100

    Add ThreeWirePacket implementation

---
+++ packet.js
@@ -0,0 +1,65 @@
+const ThreeWireHeader = require('./header');
+
+const HEADER_LENGTH = 4;
+
+/**
+ * @class ThreeWirePacket
+ * 
+ * Represents a complete Three Wire UART Protocol packet
+ * 
+ * @example
+ * import packet1 = new ThreeWirePacket(packetBytesArray);
+ */
+class ThreeWirePacket {
+    /**
+     * @param {Array} packetBytesArray The raw packet
+     */
+    constructor(packetBytesArray) {
+        this._pktBytes = packetBytesArray;
+
+        if (!packetBytesArray) {
+            throw new Error('Invalid input, undefined');
+        } else if (packetBytesArray.length < HEADER_LENGTH) {
+            throw new Error(`Invalid input, length must be >= ${HEADER_LENGTH}`);
+        }
+
+        this.parseHeader();
+        this.parsePayload();
+        //this.parseCRC();
+    }
+
+    parseHeader() {
+        this._header = new ThreeWireHeader(this._pktBytes);
+        
+        if (!this._header) {
+            throw new Error('Invalid header, could not parse');
+        }
+    }
+
+    parsePayload() {
+        const payloadStart = HEADER_LENGTH;
+
+        if ((this._pktBytes.length - HEADER_LENGTH) < this._header.payloadLength) {
+            throw new Error('Invalid length, payload array shorter than header payload length');
+        }
+
+        const payloadEnd = payloadStart + this._header.payloadLength;
+        this._payloadBytesArray = this._pktBytes.slice(payloadStart, payloadEnd);
+    }
+
+    /**
+     * @type {ThreeWireHeader} Packet header
+     */
+    get header() {
+        return this._header;
+    }
+
+    /**
+     * @type {object} Packet payload
+     */
+    get payload() {
+        return this._payloadBytesArray;
+    }
+}
+
+module.exports = ThreeWirePacket;
---
+++ packet.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const ThreeWirePacket = require('./packet');
+
+describe('ThreeWirePacket', () => {
+    const validPkt1 = [0xDE, 0x5E, 0x03, 0xC1, 0x02, 0x1D, 0x00, 0xFF, 0xFF, 0x02, 0x7D, 0x6E, 0x68,
+        0xB5, 0xFD, 0xE2, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xB4, 0x06, 0x1E, 0x01, 0x02, 0x01, 0x04, 0x1A, 0xFF, 0x59,
+        0x00, 0x02, 0x15, 0x01, 0x12, 0x23, 0x34, 0x45, 0x56, 0x67, 0x78, 0x89, 0x9A, 0xAB, 0xBC, 0xCD, 0xDE, 0xEF, 0xF0, 0x01,
+        0x02, 0x03, 0x04, 0xC3, 0x80, 0x13];
+
+    it('Can be constructed with a valid packet', () => {
+        const pkt = new ThreeWirePacket(validPkt1);
+        expect(pkt).toBeDefined();
+        expect(pkt.header).toBeDefined();
+        console.log(pkt.header.toString());
+    });
+
+    it('Throws error if packet is shorter than length field', () => {
+        const tooShortPkt = validPkt1.slice(0, validPkt1.length - 5);
+        expect(() => {
+            const pkt = new ThreeWirePacket(tooShortPkt);
+        }).toThrow();
+    });
+
+    it('Parses header correctly', () => {
+        const pkt = new ThreeWirePacket(validPkt1);
+        expect(pkt.header.packetType).toBe(14);
+        expect(pkt.header.ackNumber).toBe(3);
+        expect(pkt.header.sequenceNumber).toBe(2);
+        expect(pkt.header.validHeader).toBe(true);
+        expect(pkt.header.reliablePacket).toBe(true);
+    });
+
+    it('Parses payload correctly', () => {
+        const pkt = new ThreeWirePacket(validPkt1);
+        const crcLength = pkt.header.reliablePacket ? 2 : 0;
+        const expPayload = validPkt1.slice(4, validPkt1.length - 4 + crcLength);
+        expect(pkt.payload).toEqual(expPayload);
+    })
+});
